4882537de44d6f49785bbefc99454a0460cab96a
Henrik Johansson <henrik89johansson@gmail.com>
2016-04-14 21:15:01 +0200

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,42 @@
+var tasks = (function() {
+	var tasks = ['Lägg till ;', 'Uppdatera Permalinks'];
+
+		var $el = $('#tasksModule'),
+			$btn = $el.find('button#addTask'),
+			$input = $el.find('input'),
+			$ul = $el.find('ul#tasks'),
+			template = $el.find('#tasks-template').html();
+		
+		$btn.on('click', addTask);
+		$ul.delegate('i.del', 'click', deleteTask);
+
+		_render();
+
+		function _render() {
+			$ul.html(Mustache.render(template, {tasks: tasks}));
+		}
+		
+		function addTask(value) {
+			var task = (typeof value === 'string') ? value : $input.val();
+			tasks.push(task);
+			_render();
+			$input.val('');
+		}
+		
+		function deleteTask(event) {
+			var i;
+			if (typeof event === 'number') {
+				i = event;
+			} else {
+				var $remove = $(event.target).closest('li');
+				i = $ul.find('li').index($remove);
+			}
+			tasks.splice(i, 1);
+			_render();
+		}
+
+		return {
+			addTask: addTask,
+			deleteTask: deleteTask
+		};
+})();
